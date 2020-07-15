b3d51bfce976d8de6f59190571322033207beebf
xcH3332 <brush123z@gmail.com>
2020-07-15 21:25:01 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,13 @@
 
 
 echo -e " [**] Enabling services hold on............../\///// "
+echo -e " [**] Statrting services hold on............../\///// "
+systemctl daemon-reload
+systemctl enable wazuh-manager.service
+systemctl enable wazuh-api.service
+systemctl enable elasticsearch.service
+systemctl enable filebeat.service
+systemctl enable kibana.service
 systemctl restart wazuh-manager
 systemctl restart wazuh-api
 systemctl restart elasticsearch
